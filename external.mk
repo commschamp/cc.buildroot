ef7fd564841911824b1e057ef9cbade8e9ee9177
Alex Robenko <arobenko@gmail.com>
2022-11-04 08:07:56 +1000

    Added cc-comms package.

---
+++ external.mk
@@ -0,0 +1,1 @@
+include $(sort $(wildcard $(BR2_EXTERNAL_COMMS_CHAMP_PATH)/package/*/*.mk))
---
+++ package/cc-comms/cc-comms.mk
@@ -0,0 +1,13 @@
+################################################################################
+#
+# cc-comms
+#
+################################################################################
+
+CC_COMMS_VERSION = 5.0.1
+CC_COMMS_SITE = $(call github,commschamp,comms,v$(CC_COMMS_VERSION))
+CC_COMMS_INSTALL_STAGING = YES
+CC_COMMS_LICENSE = MPL-2
+CC_COMMS_LICENSE_FILES = LICENSE
+
+$(eval $(cmake-package))
