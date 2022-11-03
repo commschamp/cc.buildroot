d514d598690cad302487ecdf80be11c26a0180f0
Alex Robenko <arobenko@gmail.com>
2022-11-04 08:33:54 +1000

    Added cc-mqtt311 package.

---
+++ package/cc-mqtt311/cc-mqtt311.mk
@@ -0,0 +1,12 @@
+################################################################################
+#
+# cc-mqtt311
+#
+################################################################################
+
+CC_MQTT311_VERSION = 2.0
+CC_MQTT311_SITE = $(call github,commschamp,cc.mqtt311.generated,v$(CC_MQTT311_VERSION))
+CC_MQTT311_INSTALL_STAGING = YES
+CC_MQTT311_LICENSE = None
+
+$(eval $(cmake-package))
