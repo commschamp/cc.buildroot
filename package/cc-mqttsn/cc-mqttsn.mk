cd43a63953cf44682a283b551e559ecb25c25cfc
Alex Robenko <arobenko@gmail.com>
2022-11-04 08:42:15 +1000

    Addec cc-mqttsn package.

---
+++ package/cc-mqttsn/cc-mqttsn.mk
@@ -0,0 +1,12 @@
+################################################################################
+#
+# cc-mqttsn
+#
+################################################################################
+
+CC_MQTTSN_VERSION = 2.0
+CC_MQTTSN_SITE = $(call github,commschamp,cc.mqttsn.generated,v$(CC_MQTTSN_VERSION))
+CC_MQTTSN_INSTALL_STAGING = YES
+CC_MQTTSN_LICENSE = None
+
+$(eval $(cmake-package))
