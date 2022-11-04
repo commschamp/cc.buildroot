bb4f2106e7c0d6a5d3fa6d4a0d8b3819b44e3ab7
Alex Robenko <arobenko@gmail.com>
2022-11-04 16:29:34 +1000

    Added cc-x509 package.

---
+++ package/cc-x509/cc-x509.mk
@@ -0,0 +1,12 @@
+################################################################################
+#
+# cc-x509
+#
+################################################################################
+
+CC_X509_VERSION = 0.1
+CC_X509_SITE = $(call github,commschamp,cc.x509.generated,v$(CC_X509_VERSION))
+CC_X509_INSTALL_STAGING = YES
+CC_X509_LICENSE = None
+
+$(eval $(cmake-package))
