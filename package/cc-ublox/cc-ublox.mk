1a9a8164e620528bf01e5633e073cc68ccb109a7
Alex Robenko <arobenko@gmail.com>
2022-11-04 16:21:54 +1000

    Added cc-ublox package.

---
+++ package/cc-ublox/cc-ublox.mk
@@ -0,0 +1,12 @@
+################################################################################
+#
+# cc-ublox
+#
+################################################################################
+
+CC_UBLOX_VERSION = 2.0
+CC_UBLOX_SITE = $(call github,commschamp,cc.ublox.generated,v$(CC_UBLOX_VERSION))
+CC_UBLOX_INSTALL_STAGING = YES
+CC_UBLOX_LICENSE = None
+
+$(eval $(cmake-package))
