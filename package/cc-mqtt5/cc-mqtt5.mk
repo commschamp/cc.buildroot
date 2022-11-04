d3fe50c22a3f9e31424f1d5af28c4069ff0c7957
Alex Robenko <arobenko@gmail.com>
2022-11-04 16:09:12 +1000

    Added cc-mqtt5 package.

---
+++ package/cc-mqtt5/cc-mqtt5.mk
@@ -0,0 +1,12 @@
+################################################################################
+#
+# cc-mqtt5
+#
+################################################################################
+
+CC_MQTT5_VERSION = 2.0
+CC_MQTT5_SITE = $(call github,commschamp,cc.mqtt5.generated,v$(CC_MQTT5_VERSION))
+CC_MQTT5_INSTALL_STAGING = YES
+CC_MQTT5_LICENSE = None
+
+$(eval $(cmake-package))
