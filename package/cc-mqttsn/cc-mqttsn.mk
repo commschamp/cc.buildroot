b0ce8a9f8715504ef2d829234e6025db10c7ee4f
Alex Robenko <arobenko@gmail.com>
2023-03-18 13:57:26 +1000

    Using latest releases.
    cc-comms - v5.1.1
    cc-commsdsl - v6.0
    cc-mqtt5 - v2.2.1
    cc-mqtt311 - v2.2.1
    cc-mqttsn - v2.2
    cc-ublox - v2.2
    cc-x509 - v0.4

--- package/cc-mqttsn/cc-mqttsn.mk
+++ package/cc-mqttsn/cc-mqttsn.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-CC_MQTTSN_VERSION = 2.1
+CC_MQTTSN_VERSION = 2.2
 CC_MQTTSN_SITE = $(call github,commschamp,cc.mqttsn.generated,v$(CC_MQTTSN_VERSION))
 CC_MQTTSN_INSTALL_STAGING = YES
 CC_MQTTSN_LICENSE = None
--- package/cc-ublox/cc-ublox.mk
+++ package/cc-ublox/cc-ublox.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-CC_UBLOX_VERSION = 2.1
+CC_UBLOX_VERSION = 2.2
 CC_UBLOX_SITE = $(call github,commschamp,cc.ublox.generated,v$(CC_UBLOX_VERSION))
 CC_UBLOX_INSTALL_STAGING = YES
 CC_UBLOX_LICENSE = None
