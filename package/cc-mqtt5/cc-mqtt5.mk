9039e3f1db302d9f817faf7571868d00ed0f3da2
Alex Robenko <arobenko@gmail.com>
2023-04-24 08:25:37 +1000

    Using latest releases.
    cc-comms - v5.2
    cc-commsdsl - v6.1
    cc-mqtt5 - v2.3
    cc-mqtt311 - v2.3
    cc-mqttsn - v2.3
    cc-ublox - v2.3
    cc-x509 - v0.5

--- package/cc-mqtt5/cc-mqtt5.mk
+++ package/cc-mqtt5/cc-mqtt5.mk
@@ -4,10 +4,11 @@
 #
 ################################################################################
 
-CC_MQTT5_VERSION = 2.2.1
+CC_MQTT5_VERSION = 2.3
 CC_MQTT5_SITE = $(call github,commschamp,cc.mqtt5.generated,v$(CC_MQTT5_VERSION))
 CC_MQTT5_INSTALL_STAGING = YES
-CC_MQTT5_LICENSE = None
+CC_MQTT5_LICENSE = MPL-2.0
+CC_MQTT5_LICENSE_FILES = LICENSE
 CC_MQTT5_DEPENDENCIES = cc-comms
 
 $(eval $(cmake-package))
