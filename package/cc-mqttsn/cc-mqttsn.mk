9b2c868507392dc3415868752eb8b1e707173eb5
Alex Robenko <arobenko@gmail.com>
2024-04-13 15:40:14 +1000

    Updated versions:
    cc-comms - v5.2.3
    cc-commsdsl - v6.3.2
    cc-mqtt5 - v2.7
    cc-mqtt5-libs - v0.4
    cc-mqtt311 - v2.7
    cc-mqttsn - v2.7
    cc-mqttsn-libs - v1.0.8
    cc-ublox - v2.7
    cc-x509 - v0.9

--- package/cc-mqttsn/cc-mqttsn.mk
+++ package/cc-mqttsn/cc-mqttsn.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-CC_MQTTSN_VERSION = 2.6
+CC_MQTTSN_VERSION = 2.7
 CC_MQTTSN_SITE = $(call github,commschamp,cc.mqttsn.generated,v$(CC_MQTTSN_VERSION))
 CC_MQTTSN_INSTALL_STAGING = YES
 CC_MQTTSN_LICENSE = MPL-2.0
