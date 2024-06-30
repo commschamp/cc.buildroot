a88794739cfe39576d508d2f2f0a0434b77f3566
Alex Robenko <arobenko@gmail.com>
2024-06-30 11:07:09 +1000

    Updates: * comms - v5.2.5 * commsdsl - v6.3.3 * cc.mqtt311.generated - v2.8 * cc.mqttsn.generated - v2.8 * cc.mqtt5.generated - v2.8 * cc.ublox.generated - v2.8 * cc.x509.generated - v1.0 * cc.mqtt5.libs - v1.0 * cc.mqtt311.libs - v1.0

--- package/cc-comms/cc-comms.mk
+++ package/cc-comms/cc-comms.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-CC_COMMS_VERSION = 5.2.3
+CC_COMMS_VERSION = 5.2.5
 CC_COMMS_SITE = $(call github,commschamp,comms,v$(CC_COMMS_VERSION))
 CC_COMMS_INSTALL_STAGING = YES
 CC_COMMS_LICENSE = MPL-2.0
