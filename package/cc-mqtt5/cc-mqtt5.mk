96b5845bcd0ab2c9612e69c3bf6bf369c2b6d9db
Alex Robenko <arobenko@gmail.com>
2024-09-30 08:33:39 +1000

    Updates: * cc-comms - v5.2.7 * cc-commsdsl - v6.3.4 * cc-mqtt311 - v2.9 * cc-mqtt5 - v2.9 * cc-mqttsn - v2.9 * cc-mqtt311-libs - v1.0.1 * cc-mqtt5-libs - v1.0.1 * cc-mqttsn-libs - v2.0.4 * cc-ublox - v2.9 * cc-x509 - v1.0.1

--- package/cc-mqtt5/cc-mqtt5.mk
+++ package/cc-mqtt5/cc-mqtt5.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-CC_MQTT5_VERSION = 2.8
+CC_MQTT5_VERSION = 2.9
 CC_MQTT5_SITE = $(call github,commschamp,cc.mqtt5.generated,v$(CC_MQTT5_VERSION))
 CC_MQTT5_INSTALL_STAGING = YES
 CC_MQTT5_LICENSE = MPL-2.0
